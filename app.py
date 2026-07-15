a1920a12e2e1aa522161600ec684a39691feb295
gangthadepv@gmail.com <gangthadepv@gmail.com>
2026-07-16 00:50:37 +0530

    tested llm for single query

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ load_dotenv()
 from langchain_google_genai import ChatGoogleGenerativeAI
 llm=ChatGoogleGenerativeAI(model="gemini-3.5-flash")
 
-
-response=llm.invoke("what is machine learning")
+response=llm.invoke("what is self attention")
 print(response.content)
